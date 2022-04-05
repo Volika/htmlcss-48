ad69ab2fbb28073ed94d55a6a0f65d4d12b376f2
Valeriia <volikavs@gmail.com>
2022-04-05 18:37:25 +0300

    modal

--- js/our-location-modal.js
+++ js/our-location-modal.js
@@ -5,11 +5,29 @@
     modal: document.querySelector('[data-modal-map]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  const maxWidth = document.body.clientWidth;
 
-  function toggleModal() {
-    document.body.classList.toggle('modal-map-open');
-    refs.modal.classList.toggle('is-hidden');
+  // refs.openModalBtn.addEventListener('click', toggleModal);
+  // refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.openModalBtn.addEventListener('click', addModal);
+  refs.closeModalBtn.addEventListener('click', removeModal);
+
+  function addModal() {
+    // document.body.classList.add('modal-map-open');
+    refs.modal.classList.remove('is-hidden');
+    // console.log('ddddddddddddddddddd');
+    document.body.setAttribute('style', `max-width: ${maxWidth}px; overflow: hidden;`);
   }
+
+  function removeModal() {
+    document.body.classList.remove('modal-map-open');
+    refs.modal.classList.add('is-hidden');
+    document.body.removeAttribute('style');
+  }
+
+  // function toggleModal() {
+  //   document.body.classList.toggle('modal-map-open');
+  //   refs.modal.classList.toggle('is-hidden');
+  // }
 })();
